e140c3ceaf5aeb602deaae2fdd5aa1e30c57a009
XuDong Wu <118393772+OrangeSpace0721@users.noreply.github.com>
2025-05-03 23:59:24 +0100

    Update GCD_train.sh
    Changed train script to match my directory

--- GCD_train.sh
+++ GCD_train.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -l
 #SBATCH -p gpu
-#SBATCH --mem=32G
+#SBATCH --mem=16G
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH -c 32
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=as1g21@soton.ac.uk
-#SBATCH --time=24:0:00
+#SBATCH --mail-user=xw3g19@soton.ac.uk
+#SBATCH --time=48:0:00
 
 module load conda/py3-latest
 conda activate GCD-env
 
-python train.py --dump_path /home/as1g21/GCD/results --exp_name DEMO --base 10 --maxint 1000000 --env_base_seed 42
+python train.py --dump_path /ECShome/xw3g19/Reproduce_GCD_LLM/GCD/results --exp_name LCM --n_enc_layers 2 --n_dec_layers 2 --batch_size 512 --base 10 --maxint 1000000 --env_base_seed 42
